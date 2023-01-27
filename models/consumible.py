7d1855b84b72f63984ae18bc2ef7815bb80a31d4
José Ramón López Guillén <jose.rlg27@gmail.com>
2023-01-27 14:22:45 +0100

    Creadas operaciones en revisión y modificada las acl

--- models/consumible.py
+++ models/consumible.py
@@ -4,23 +4,30 @@ class Consumible(models.Model):
     _name = 'consumible'
     _description = 'Consumibles de Garaje GV'
 
-    codigo = fields.Integer('Código producto')
-    nombre = fields.Char('Nombre producto')
+    codigo = fields.Integer('Código producto', required=True)
+    nombre = fields.Char('Nombre producto', required=True)
     descripcion = fields.Html('Descripción', sanitize=True, strip_style=False)
     disponible = fields.Selection([
         ('si','Disponible'),
         ('no','No disponible')
-        ],'Disponible',default='si')
+        ],'Disponible',default='si', required=True)
     base_i= fields.Float ('Base Imponible')
-    iva = fields.Integer('IVA')
+    #iva = fields.Integer('IVA')
+    iva = fields.Selection([
+            ('4','4% Superreducido'),
+            ('10','10% Reducido'),
+            ('21','21% General')
+            ],'IVA',default='21', required=True)
     pvp = fields.Float('Pvp', default = 0.0, compute = '_calculate_pvp')
 
-    _read_name = nombre
+    _rec_name='nombre'
 
     @api.onchange('base_i','iva')
     def _calculate_pvp(self):
         for record in self:
             if record.base_i and record.base_i > 0:
-                record.pvp = record.base_i * ((record.iva / 100) + 1)
+                record.pvp = record.base_i * ((int(record.iva) / 100) + 1)
+            else:
+                record.pvp = 0    
 
 
--- models/revision.py
+++ models/revision.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 
@@ -14,7 +14,20 @@ class Revision(models.Model):
      kml_p = fields.Integer('Kilómetros próxima')
      fecha = fields.Date('Fecha', default = lambda self:fields.Date.today())
      horas_taller = fields.Integer('Horas taller')
-     valor_total = fields.Float('Total')
+     valor_total = fields.Float('Total',compute='_add_prices_to_total')
      bastidor = fields.Many2one('vehiculo')   
-     mecanico = fields.Many2one('mecanico')  
+     mecanico = fields.Many2one('mecanico')
+     consumibles = fields.Many2many('consumible',string='Consumbles')  
      descripcion = fields.Html('Descripción', sanitize=True, strip_style=False)
+        
+
+     @api.onchange('consumibles','horas_taller')
+     def _add_prices_to_total(self):
+          precio_hora = 25
+          for r in self:
+               r.valor_total = 0
+               for item in r.consumibles:
+                    r.valor_total = r.valor_total + item.pvp
+               if r.horas_taller and r.horas_taller > 0:
+                    r.valor_total = r.valor_total + (r.horas_taller * precio_hora)
+               
